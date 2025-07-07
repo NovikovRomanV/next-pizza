b2d9b76ef1b76b3900ce459baec9b930fe96210a
NovikovRomanV <rimm19845@gmail.com>
2025-07-07 20:55:31 +0300

    feat: category.ts, product-card.tsx, products-group-list.tsx fix: categories.tsx,  page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import {Container, Filters, Title, TopBar} from "@/components/shared";
+import {ProductsGroupList} from "@/components/shared/products-group-list";
 
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
             </Container>
             <TopBar/>
             <Container className="mt-10 pb-14">
-                <div className="flex gap-[68px]">
+                <div className="flex gap-[80px]">
 
                     {/*фильтр*/}
                     <div className="w-[250px]">
@@ -19,7 +20,80 @@ export default function Home() {
                     {/*список товаров*/}
                     <div className="flex-1">
                         <div className="flex flex-col gap-16">
-                            Список товаров
+                            <ProductsGroupList title='Пиццы' items={[
+                                {
+                                    id:0,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif',
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:1,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:2,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:3,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:4,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                }
+                            ]} categoryId={1}/>
+                            <ProductsGroupList title='Комбо' items={[
+                                {
+                                    id:0,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: 'https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif',
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:1,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:2,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:3,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                },
+                                {
+                                    id:4,
+                                    name: "Пицца моцарелла",
+                                    price: 500,
+                                    imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d612fc7b7fca5be822752bee1e5.avif",
+                                    items: [{price: 550}],
+                                }
+                            ]} categoryId={2}/>
                         </div>
                     </div>
                 </div>
